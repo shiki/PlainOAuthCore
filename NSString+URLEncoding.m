ab9719da66dcec86f4c6f698e236b7c938f38fb3
Shiki <shikishiji@gmail.com>
2013-02-02 21:43:21 +0800

    Fix for NSString+URLEncoding causing memory leaks.

--- NSString+URLEncoding.m
+++ NSString+URLEncoding.m
@@ -29,33 +29,35 @@
 @implementation NSString (OAURLEncodingAdditions)
 
 - (NSString *)encodedURLString {
-	NSString *result = (__bridge NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
-                                                                                  (__bridge CFStringRef)self,
-                                                                                  NULL,                   // characters to leave unescaped (NULL = all escaped sequences are replaced)
-                                                                                  CFSTR("?=&+"),          // legal URL characters to be escaped (NULL = all legal characters are replaced)
-                                                                                  kCFStringEncodingUTF8); // encoding
-  return result;
+	NSString *result = (__bridge_transfer NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+                                                                                           (__bridge CFStringRef)self,
+                                                                                           NULL,                   // characters to leave unescaped (NULL = all escaped sequences are replaced)
+                                                                                           CFSTR("?=&+"),          // legal URL characters to be escaped (NULL = all legal characters are replaced)
+                                                                                           kCFStringEncodingUTF8); // encoding
+	return result;
 }
 
 - (NSString *)encodedURLParameterString {
-  NSString *result = (__bridge NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
-                                                                                  (__bridge CFStringRef)self,
-                                                                                  NULL,
-                                                                                  CFSTR(":/=,!$&'()*+;[]@#?"),
-                                                                                  kCFStringEncodingUTF8);
-  return result;
+  NSString *result = (__bridge_transfer NSString *)CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+                                                                                           (__bridge CFStringRef)self,
+                                                                                           NULL,
+                                                                                           CFSTR(":/=,!$&'()*+;[]@#?"),
+                                                                                           kCFStringEncodingUTF8);
+	return result;
 }
 
 - (NSString *)decodedURLString {
-	NSString *result = (__bridge NSString*)CFURLCreateStringByReplacingPercentEscapesUsingEncoding(kCFAllocatorDefault,
-                                                                                                 (__bridge CFStringRef)self,
-                                                                                                 CFSTR(""),
-                                                                                                 kCFStringEncodingUTF8);
-	
-  return result;
+	NSString *result = (__bridge_transfer NSString*)CFURLCreateStringByReplacingPercentEscapesUsingEncoding(kCFAllocatorDefault,
+                                                                                                          (__bridge CFStringRef)self,
+                                                                                                          CFSTR(""),
+                                                                                                          kCFStringEncodingUTF8);
+
+	return result;
+
 }
 
-- (NSString *)removeQuotes {
+-(NSString *)removeQuotes
+{
 	NSUInteger length = [self length];
 	NSString *ret = self;
 	if ([self characterAtIndex:0] == '"') {
@@ -64,7 +66,7 @@
 	if ([self characterAtIndex:length - 1] == '"') {
 		ret = [ret substringToIndex:length - 2];
 	}
-	
+
 	return ret;
 }
 
